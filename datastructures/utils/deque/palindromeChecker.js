9b2b01778c7bb3c6f388f061d75e14ec8dcd9947
Matheus Rian <souzamatheusrian@gmail.com>
2022-02-27 22:32:55 -0300

    feat: palindrome checker deque

---
+++ datastructures/utils/deque/palindromeChecker.js
@@ -0,0 +1,30 @@
+import { Deque } from "../../deque/deque.js";
+
+function palindromeChecker(aString) {
+  if (aString === null || aString === undefined ||
+    (aString !== null && aString.length === 0)) {
+    return false;
+  }
+
+  const deque = new Deque();
+  const lowerString = aString.toLocaleLowerCase().split(' ').join('');
+  let isEqual = true;
+  let firstChar, lastChar;
+  for (let i = 0; i < lowerString.length; i++) {
+    deque.addBack(lowerString.charAt(i));
+  }
+
+  while (deque.size() > 1 && isEqual) {
+    firstChar = deque.removeFront();
+    lastChar = deque.removeBack();
+
+    if (firstChar !== lastChar) {
+      isEqual = false;
+    }
+  }
+
+  return isEqual;
+}
+
+console.log('a', palindromeChecker('a'));
+console.log('level', palindromeChecker('level'));
